7bbe631320fb2baf2b64a5aa765e7d5678a8b404
blakeknox <ex0916tq@my.hennepintech.edu>
2017-02-27 13:03:57 -0600

    made the new XAML and the xamerian match

--- Family/Family/FamilyPage.cs
+++ Family/Family/FamilyPage.cs
@@ -12,49 +12,69 @@ namespace Family
         public FamilyPage()
         {
 
-            BackgroundColor = Color.Aqua;
-            Content = new Label
+            BackgroundColor = Color.Black;
+            var stacklayout = new StackLayout
             {
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-              
-                FormattedText = new FormattedString
+
+                Orientation = StackOrientation.Vertical,
+
+                Children =
                 {
-                    Spans =
-                    {
-                        new Span
+                        new Label
                         {
                             Text = "Greetings, and Welcome to my family app. My Family Memebers are:",
-                             FontAttributes = FontAttributes.Bold
-
+                             FontAttributes = FontAttributes.Bold,
+                             VerticalOptions = LayoutOptions.FillAndExpand,
+                             HorizontalOptions = LayoutOptions.Center,
+                             TextColor = Color.Maroon,
+                             BackgroundColor = Color.White,
+                             FontSize = 36
                         },
-                        new Span
+                        new Label
                         {
-                            Text = Environment.NewLine + "Blake",
+                            Text = "Blake - That is me and I am the father of the family. I am currently a CNC Machinist, and have been for the last 6 years. I should be done with school this fall.",
                             FontAttributes = FontAttributes.Bold,
-                            ForegroundColor = Color.Red,
-
+                            VerticalOptions = LayoutOptions.FillAndExpand,
+                            HorizontalOptions = LayoutOptions.Center,
+                            TextColor = Color.Red,
+                            FontSize = 24
+                        },
+                        new Label
+                        {
+                            Text = "Chelsea - Is the mother and she is also in college. She is in her last class right now.",
+                            //Text = Environment.NewLine + "Chelsea - Is the mother and she is also in college. She is in her last class right now.",
+                            VerticalOptions = LayoutOptions.FillAndExpand,
+                            HorizontalOptions = LayoutOptions.Center,
+                            BackgroundColor = Color.Green,
+                            FontSize = 28
+                        },
+                        new Label
+                        {
+                            Text = "Karson - He is our 6 year old son, and the absolute bright spot in our lifes. He is in first grade and reading like a champ. He is my mini me and that may be a bad thing in some ways, but he is perfect.",
+                            FontAttributes = FontAttributes.Bold,
+                            VerticalOptions = LayoutOptions.FillAndExpand,
+                            HorizontalOptions = LayoutOptions.Center,
+                            BackgroundColor = Color.Yellow,
+                            TextColor = Color.Green,
+                            FontSize = 34 //Device.GetNamedSize(NamedSize.Large, typeof(Label))
 
                         },
-                        new Span
+                        new Label
                         {
-                            Text = Environment.NewLine + "Chelsea",
-                            FontAttributes = FontAttributes.Italic,
-                            ForegroundColor = Color.Green
-
-                        },
-                        new Span
-                        {
-                            Text = Environment.NewLine + "Karson",
-                            FontAttributes = FontAttributes.None,
-                            ForegroundColor = Color.Blue,
-                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+                            Text = "Tyke - He is a rescued dog. We got him from a rescue house that my step-sister runs. We got lucky because he is truely a one in a million animal.",
+                            VerticalOptions = LayoutOptions.FillAndExpand,
+                            HorizontalOptions = LayoutOptions.FillAndExpand,
+                            FontAttributes = FontAttributes.Bold,
+                            FontSize = 20 //Device.GetNamedSize(NamedSize.Large, typeof(Label))
 
                         }
-                    }  
-                }
-                
 
+                }
+            };
+            Content = new ScrollView
+            {
+                Orientation = ScrollOrientation.Vertical,
+                Content = stacklayout
             };
 
         }
